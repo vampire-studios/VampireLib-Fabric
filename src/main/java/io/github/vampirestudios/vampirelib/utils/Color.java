d066489641992f880d29e5fc42111caed802a7bd
oliviathevampire <sindrefagerheim53@gmail.com>
2020-02-02 00:12:44 +0100

    updated license

--- src/main/java/io/github/vampirestudios/vampirelib/utils/Color.java
+++ src/main/java/io/github/vampirestudios/vampirelib/utils/Color.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019 Vampire Studios
+ * Copyright (c) 2020 Vampire Studios
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
