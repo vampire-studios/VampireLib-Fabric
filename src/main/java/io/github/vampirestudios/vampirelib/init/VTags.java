00f36feb524d535f3efeb009baf0cefdd228ef4e
OliviaTheVampire <sindrefagerheim53@gmail.com>
2020-11-11 07:36:35 +0100

    removed item overlays for now

--- src/main/java/io/github/vampirestudios/vampirelib/init/VTags.java
+++ src/main/java/io/github/vampirestudios/vampirelib/init/VTags.java
@@ -20,7 +20,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *//*
+ */
+
 
 
 package io.github.vampirestudios.vampirelib.init;
@@ -40,4 +41,3 @@ public class VTags {
     }
 
 }
-*/
