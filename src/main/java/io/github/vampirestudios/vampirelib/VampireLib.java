9ceacc474185bdacb68db6a2d2832d30a4de242e
OliviaTheVampire <sindrefagerheim53@gmail.com>
2021-07-14 02:31:31 +0200

    forgot to update this

--- src/main/java/io/github/vampirestudios/vampirelib/VampireLib.java
+++ src/main/java/io/github/vampirestudios/vampirelib/VampireLib.java
@@ -696,7 +696,7 @@ public class VampireLib implements ModInitializer {
     public static String MOD_ID = "vampirelib";
     public static String MOD_NAME = "VampireLib";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
-    public static String MOD_VERSION = "3.0.3+build.1 for 1.17";
+    public static String MOD_VERSION = "3.1.0+build.2 for 1.17.1";
 
     @Override
     public void onInitialize() {
